cbe4c893e7a0b6e691a72ca3505a8bc1f508bb86
mnsekh111 <mnsekh111@gmail.com>
2015-11-12 02:56:48 -0500

    Added Text hightlight and Text entry 2. Changed to full screen

--- app/src/main/java/com/mns/myapplication/RaceActivity.java
+++ app/src/main/java/com/mns/myapplication/RaceActivity.java
@@ -1,37 +1,117 @@
 package com.mns.myapplication;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.text.Editable;
+import android.text.Spannable;
+import android.text.TextWatcher;
+import android.text.style.BackgroundColorSpan;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RaceActivity extends Activity {
+
+    private EditText etInput;
+    private TextView tvPassage, tvWpm, tvPosition, tvAcc;
+
+
+    private volatile int timeElapsed = 0;
+    private int currentWord = 0;
+    private boolean hasCompleted = false;
+    private String[] passageWords;
+    private String passage;
+    private Spannable spanText = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_race);
+        initViews();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_race, menu);
-        return true;
+    private void initViews() {
+        etInput = (EditText) findViewById(R.id.etInput);
+        etInput.addTextChangedListener(mInpWatcher);
+
+        tvPassage = (TextView) findViewById(R.id.tvRacePassage);
+        tvWpm = (TextView) findViewById(R.id.tvWPM);
+        tvPosition = (TextView) findViewById(R.id.tvPosition);
+        tvAcc = (TextView) findViewById(R.id.tvAcc);
+
+        setPassage("My name is Sekharan. I'm a good boy.");
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private TextWatcher mInpWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            if (!mTimerThread.isAlive()) {
+                mTimerThread.start();
+            }
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            if (s.length() >= 1 && s.charAt(s.length() - 1) == ' ') {
+                Log.i("COMP", passageWords[currentWord] + "-->" + s.toString().substring(0, s.length() - 1));
+                if ((passageWords[currentWord] + " ").contentEquals(s.toString().substring(0, s.length()))) {
+
+                    etInput.setText("");
+                    int start = 0;
+
+                    if (currentWord < passageWords.length - 1) {
+                        currentWord++;
+                        for (int i = 0; i < currentWord; i++) {
+                            start += (passageWords[i].length() + 1);
+                        }
+                        spanText = Spannable.Factory.getInstance().newSpannable(passage);
+                        spanText.setSpan(new BackgroundColorSpan(0xFFFFFF00), start, start + passageWords[currentWord].length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                        tvPassage.setText(spanText);
+                        etInput.setTextColor(Color.BLACK);
+                    }
+                    else{
+                        //Show the complete activity
+                    }
+                } else {
+                    etInput.setTextColor(Color.RED);
+                }
+            }
+        }
+    };
+
+
+    private Thread mTimerThread = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            try {
+
+                while (!hasCompleted) {
+                    Thread.sleep(1000);
+                    timeElapsed++;
+                }
+            } catch (InterruptedException ie) {
+
+            }
+        }
+    });
+
+    private void setPassage(String str) {
+        if (tvPassage != null) {
+            passage = str;
+            passageWords = passage.split(" ");
+            currentWord = 0;
+            spanText = Spannable.Factory.getInstance().newSpannable(passage);
+            spanText.setSpan(new BackgroundColorSpan(0xFFFFFF00), 0
+                    , passageWords[currentWord].length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            tvPassage.setText(spanText);
+        }
     }
 }
