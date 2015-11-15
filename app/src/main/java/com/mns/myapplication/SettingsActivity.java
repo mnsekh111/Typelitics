5f690e0b6dcd5f2179bf8f6947079f44842b65aa
Sekharan Natarajan <mnsekh111@gmail.com>
2015-11-14 20:05:01 -0500

    Added getPreferences + cleaned up strings

--- app/src/main/java/com/mns/myapplication/SettingsActivity.java
+++ app/src/main/java/com/mns/myapplication/SettingsActivity.java
@@ -1,16 +1,52 @@
 package com.mns.myapplication;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.text.InputType;
+import android.util.Log;
 
-public class SettingsActivity extends PreferenceActivity
-{
+public class SettingsActivity extends PreferenceActivity {
+
+    private SharedPreferences prefs;
 
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.user_settings);
+        logPrefs();
     }
+
+
+    public Settings getPreferences() {
+        Settings s = new Settings();
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        s.auto_complete = prefs.getBoolean("auto_complete", false);
+        s.auto_correct = prefs.getBoolean("auto_correct", false);
+        s.suggest = prefs.getBoolean("suggest", false);
+        s.keyboard = Integer.parseInt(prefs.getString("keyboard","0"));
+
+        return s;
+
+    }
+
+    private class Settings {
+        boolean auto_correct;
+        boolean auto_complete;
+        boolean suggest;
+        int keyboard;
+    }
+
+    private void logPrefs(){
+        Settings s = getPreferences();
+        Log.i("Pref","Auto Complete "+s.auto_complete);
+        Log.i("Pref","Auto Correct  "+s.auto_correct);
+        Log.i("Pref","Suggest "+s.suggest);
+        Log.i("Pref","keyboard " + s.keyboard);
+    }
+
 
 }
