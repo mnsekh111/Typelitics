964f8d18e9a75c7458acf4a4426fbdd9373568da
Sekharan Natarajan <mnsekh111@gmail.com>
2015-11-24 18:28:45 -0500

    Added string for Stats Activity. Replaced common fragment with different fragments

--- app/src/main/java/com/mns/myapplication/StatsActivity.java
+++ app/src/main/java/com/mns/myapplication/StatsActivity.java
@@ -3,6 +3,7 @@ package com.mns.myapplication;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
+import com.mns.myapplication.fragments.LeaderFragment;
 import com.mns.myapplication.fragments.Stats1Fragment;
 
 import java.util.ArrayList;
@@ -23,22 +24,18 @@ public class StatsActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
 
-        list1 = new ArrayList();
-        list1.add("SHIT");
-        list1.add("SHIT");
-        list1.add("SHIT");
-
 
         list2 = new ArrayList();
-        list2.add("SHIT");
-        list2.add("SHIT");
-        list2.add("SHIT");
+
 
         viewPager = (ScrollerViewPager) findViewById(R.id.view_pager);
         SpringIndicator springIndicator = (SpringIndicator) findViewById(R.id.indicator);
 
         PagerModelManager manager = new PagerModelManager();
-        manager.addCommonFragment(Stats1Fragment.class, list1, list2);
+        //manager.addCommonFragment(Stats1Fragment.class, list1, list2);
+        manager.addFragment(new Stats1Fragment(),getResources().getString(R.string.statsWPMVsGame));
+        manager.addFragment(new LeaderFragment(),getResources().getString(R.string.statsPosVsGame));
+        manager.addFragment(new Stats1Fragment(),getResources().getString(R.string.statsAccVsGame));
         ModelPagerAdapter adapter = new ModelPagerAdapter(getSupportFragmentManager(), manager);
         viewPager.setAdapter(adapter);
         viewPager.fixScrollSpeed();
